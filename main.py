e15c59bda248487ddb01c035705c956a640f1ad8
venkata-sandeep-addala <168465210+venkata-sandeep-addala@users.noreply.github.com>
2026-05-07 22:46:08 +0530

    Implement asynchronous main function and integrate math server; update dependencies in pyproject.toml and uv.lock

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from langchain-mcp!")
+import asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
+stdio_server_params = StdioServerParameters(
+    command="Python", args=["D:\\Projects\\LangChain-MCP\\servers\\math_server.py"]
+)
+
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("Session Intialized")
+            
+            tools = await load_mcp_tools(session)
+            print("\n\n")
+            
+            agent = create_agent(llm, tools)
+            
+            response = await agent.ainvoke({"messages": [HumanMessage(content="what is 54+ 3 * 2?")]})
+            print(response['messages'][-1].content)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
